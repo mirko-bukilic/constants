8ae0c419c553c649b8545fa68d778481fab461ce
Mirko Bukilic <mirko.bukilic@codeplicity.com>
2025-02-21 08:43:55 +0100

    Added missing http codes

--- src/Http.php
+++ src/Http.php
@@ -17,6 +17,8 @@ class Http
     // Informational 1xx
     const CODE_100 = 100;
     const CODE_101 = 101;
+    const CODE_102 = 102;
+    const CODE_103 = 103;
 
     // Success 2xx
     const CODE_200 = 200;
@@ -57,7 +59,17 @@ class Http
     const CODE_415 = 415;
     const CODE_416 = 416;
     const CODE_417 = 417;
+    const CODE_418 = 418;
+    const CODE_421 = 421;
     const CODE_422 = 422;
+    const CODE_423 = 423;
+    const CODE_424 = 424;
+    const CODE_425 = 425;
+    const CODE_426 = 426;
+    const CODE_428 = 428;
+    const CODE_429 = 429;
+    const CODE_431 = 431;
+    const CODE_451 = 451;
 
     // Server Error 5xx
     const CODE_500 = 500;
@@ -66,12 +78,19 @@ class Http
     const CODE_503 = 503;
     const CODE_504 = 504;
     const CODE_505 = 505;
+    const CODE_506 = 506;
+    const CODE_507 = 507;
+    const CODE_508 = 508;
     const CODE_509 = 509;
+    const CODE_510 = 510;
+    const CODE_511 = 511;
 
     // HTTP RESPONSE MESSAGES
     // Informational 1xx
     const MESSAGE_100 = 'Continue';
     const MESSAGE_101 = 'Switching Protocols';
+    const MESSAGE_102 = 'Processing';
+    const MESSAGE_103 = 'Early Hints';
 
     // Success 2xx
     const MESSAGE_200 = 'OK';
@@ -90,6 +109,7 @@ class Http
     const MESSAGE_304 = 'Not Modified';
     const MESSAGE_305 = 'Use Proxy';
     const MESSAGE_307 = 'Temporary Redirect';
+    const MESSAGE_308 = 'Permanent Redirect';
 
     // Client Error 4xx
     const MESSAGE_400 = 'Bad Request';
@@ -110,7 +130,17 @@ class Http
     const MESSAGE_415 = 'Unsupported Media Type';
     const MESSAGE_416 = 'Requested Range Not Satisfiable';
     const MESSAGE_417 = 'Expectation Failed';
+    const MESSAGE_418 = 'I am a teapot';
+    const MESSAGE_421 = 'Misdirected Request';
     const MESSAGE_422 = 'Unprocessable Entity';
+    const MESSAGE_423 = 'Locked';
+    const MESSAGE_424 = 'Failed Dependency';
+    const MESSAGE_425 = 'Too Early';
+    const MESSAGE_426 = 'Upgrade Required';
+    const MESSAGE_428 = 'Precondition Required';
+    const MESSAGE_429 = 'Too Many Requests';
+    const MESSAGE_431 = 'Request Header Fields Too Large';
+    const MESSAGE_451 = 'Unavailable For Legal Reasons';
 
     // Server Error 5xx
     const MESSAGE_500 = 'Internal Server Error';
@@ -119,7 +149,12 @@ class Http
     const MESSAGE_503 = 'Service Unavailable';
     const MESSAGE_504 = 'Gateway Timeout';
     const MESSAGE_505 = 'HTTP Version Not Supported';
+    const MESSAGE_506 = 'Variant Also Negotiates';
+    const MESSAGE_507 = 'Insufficient Storage';
+    const MESSAGE_508 = 'Loop Detected';
     const MESSAGE_509 = 'Bandwidth Limit Exceeded';
+    const MESSAGE_510 = 'Not Extended';
+    const MESSAGE_511 = 'Network Authentication Required';
 
     /**
      * @var array
@@ -127,6 +162,8 @@ class Http
     public static $messages = [
         self::CODE_100 => self::MESSAGE_100,
         self::CODE_101 => self::MESSAGE_101,
+        self::CODE_102 => self::MESSAGE_102,
+        self::CODE_103 => self::MESSAGE_103,
         self::CODE_200 => self::MESSAGE_200,
         self::CODE_201 => self::MESSAGE_201,
         self::CODE_202 => self::MESSAGE_202,
@@ -141,6 +178,7 @@ class Http
         self::CODE_304 => self::MESSAGE_304,
         self::CODE_305 => self::MESSAGE_305,
         self::CODE_307 => self::MESSAGE_307,
+        self::CODE_308 => self::MESSAGE_308,
         self::CODE_400 => self::MESSAGE_400,
         self::CODE_401 => self::MESSAGE_401,
         self::CODE_402 => self::MESSAGE_402,
@@ -159,13 +197,28 @@ class Http
         self::CODE_415 => self::MESSAGE_415,
         self::CODE_416 => self::MESSAGE_416,
         self::CODE_417 => self::MESSAGE_417,
+        self::CODE_418 => self::MESSAGE_418,
+        self::CODE_421 => self::MESSAGE_421,
         self::CODE_422 => self::MESSAGE_422,
+        self::CODE_423 => self::MESSAGE_423,
+        self::CODE_424 => self::MESSAGE_424,
+        self::CODE_425 => self::MESSAGE_425,
+        self::CODE_426 => self::MESSAGE_426,
+        self::CODE_428 => self::MESSAGE_428,
+        self::CODE_429 => self::MESSAGE_429,
+        self::CODE_431 => self::MESSAGE_431,
+        self::CODE_451 => self::MESSAGE_451,
         self::CODE_500 => self::MESSAGE_500,
         self::CODE_501 => self::MESSAGE_501,
         self::CODE_502 => self::MESSAGE_502,
         self::CODE_503 => self::MESSAGE_503,
         self::CODE_504 => self::MESSAGE_504,
         self::CODE_505 => self::MESSAGE_505,
+        self::CODE_506 => self::MESSAGE_506,
+        self::CODE_507 => self::MESSAGE_507,
+        self::CODE_508 => self::MESSAGE_508,
         self::CODE_509 => self::MESSAGE_509,
+        self::CODE_510 => self::MESSAGE_510,
+        self::CODE_511 => self::MESSAGE_511,
     ];
 }
